97be5655149384dd1e213f37e9066771bf083098
fabiCoseglia <fabiancoseglia@gmail.com>
2022-07-30 19:13:38 -0300

    add tp pimienta y sal

---
+++ data/products.js
@@ -0,0 +1,35 @@
+const products = [
+    {
+        id: 1,
+        title: 'Carpaccio fresco',
+        description: 'Entrada Carpaccio de salmón con cítricos U$S 65.50',
+        price: 65.50
+    },
+    {
+        id: 2,
+        title: 'Risotto de berenjena',
+        description: 'Risotto de berenjena y queso de cabra',
+        price: 47.00
+    },
+    {
+        id: 3,
+        title: 'Mousse de arroz',
+        description: 'Mousse de arroz con leche y aroma de azahar',
+        price: 27.50
+    },
+    {
+        id: 4,
+        title: 'Espárragos blancos',
+        description: 'Espárragos blancos con vinagreta de verduras y jamón ibérico',
+        price: 37.50
+    },
+    {
+        id: 5,
+        title: 'un asadito',
+        description: 'asado a leña y un buen fernet',
+        price: 50.00
+    }
+
+] 
+
+module.exports = products;
